ed995b006fd466276f940b296e3d0139bee47953
jmseaman <joshua.m.seaman@outlook.com>
2017-02-14 09:51:43 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix stores supplied matrix to cache
+## cacheSolve displays the inverse of a supplied matrix
+## either from cache or calculates new
 
-## Write a short comment describing this function
+## Instructions:
+## Create a matrix and store to an object for example:
+## mynewmatrix <- matrix(rnorm(1:9),3,3)
+## Make the mynewmatrix available as object in cache through
+## makeCacheMatrix, example:
+## cachedmatrix1 <- makeCacheMatrix(mynewmatrix)
+## Invert and store inverse of cachedmatrix1 using the 
+## cacheSolve function, example:
+## cacheSolve(cachedmatrix1)
+## This should display the inverse of the cachedmatrix1
+## Executing this command a second time will draw the inverse
+## from system cache
+
+## makeCacheMatrix stores square matrix to cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## cacheSolve displays the inverse of a supplied matrix
+## either from cache or calculates new
 
-## Write a short comment describing this function
+## Per assignment, function assumes it will be supplied a 
+## square matrix, i.e. 2x2 or 3x3.
+
+## Supplied matrixes must be in the form of an object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
